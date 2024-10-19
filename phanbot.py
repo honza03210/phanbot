aaf8051aedc1ab38a2fcb0699dbd0a19809e59b4
honza03210 <honzaaltrichter@gmail.com>
2024-10-19 11:34:11 +0200

    asd

--- phanbot.py
+++ phanbot.py
@@ -56,8 +56,8 @@ async def on_message(message):
             elif message.content.lower() == "ping":
                 await message.channel.send("bot " + str(bot_id) + " says hi! :D")
                 await message.channel.send("we are on channel: " + str(message.channel.id))
-                cc = client.get_channel(TRUSTED_CHANNEL)
-                await message.channel.send(cc)
+                cc = client.get_channel(int(TRUSTED_CHANNEL))
+                await message.channel.send(str(type(cc)))
                 await message.channel.send(str(TRUSTED_CHANNEL))
 
             elif message.content.lower() == "help":
