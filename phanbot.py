1534282f77c1067b46a60dbb8d07634d68900b87
honza03210 <honzaaltrichter@gmail.com>
2024-10-25 14:53:10 +0200

    dsfds

--- phanbot.py
+++ phanbot.py
@@ -125,10 +125,12 @@ async def on_message(message):
                 os.system("python3 phanbot.py " + str(bot_id + 1) + " &")
             elif message.content.lower() == "ping":
                 await message.channel.send("bot " + str(bot_id) + " says hi! :D")
-
             elif message.content.lower() == "!phantop":
                 await message.channel.send("sent to print")
                 await print_leaderboard(message.channel)
+            elif message.content.lower() == "kill":
+                client.close()
+                exit(0)
             elif message.content.lower() == "help":
                 await message.channel.send("reboot\npull\nupdate\nping\n")
 
