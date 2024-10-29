8b814ce6edf460dca138e247a299ce1547fed862
honza03210 <honzaaltrichter@gmail.com>
2024-10-29 16:52:45 +0100

    sadas

--- phanbot.py
+++ phanbot.py
@@ -130,7 +130,7 @@ async def on_raw_reaction_add(payload):
     message = await channel.fetch_message(payload.message_id)
     user_react_count = 0
     for reaction in message.reactions:
-        users = await reaction.users().flatten()
+        users = (await reaction.users()).flatten()
         if payload.user_id in users:
             user_react_count += 1
     if user_react_count > 3:
