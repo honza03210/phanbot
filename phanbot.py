298c4ce105e77515a3824c71b4b19b8c774a0c3c
honza03210 <45127038+honza03210@users.noreply.github.com>
2024-11-03 14:44:46 +0100

    Update phanbot.py

--- phanbot.py
+++ phanbot.py
@@ -252,10 +252,10 @@ async def phanbomb(trigger: str = 'idk'):
     for i, (since_last, user_id, phanpoints) in enumerate(tuples):
         user = await client.fetch_user(user_id)
         users.append(user)
-        reactions_data[user_id]['phanpoints'] += max(0, reward)
+        reactions_data[user_id]['phanpoints'] = reward + reactions_data[user_id].get('phanpoints', 0)
         reactions_data[user_id]['phanbomb'] = reactions_data[user_id].get('phanbomb', 0) + since_last
-        await user.send(f"PhanBomba vybuchlaaa, protoze {trigger}.\n Umistil/a ses na {i + 1}. miste z {len(tuples)}, od posledni PhanBomby jsi dal/a PhanTomovi {since_last} reakci.\n Dostavas tedy +{reward} PhanPointu (ted mas {phanpoints + reward})\nTakto ted vypada PhanBoard:")
-        await print_leaderboard(user)
+        #await user.send(f"PhanBomba vybuchlaaa, protoze {trigger}.\n Umistil/a ses na {i + 1}. miste z {len(tuples)}, od posledni PhanBomby jsi dal/a PhanTomovi {since_last} reakci.\n Dostavas tedy +{max(0, reward)} PhanPointu (ted mas {phanpoints + reward})\nTakto ted vypada PhanBoard:")
+        #await print_leaderboard(user)
         reward -= 1
     save_reactions()
 
